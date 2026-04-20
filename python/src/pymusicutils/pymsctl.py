56f4894a344752b94d8f2b933a0d7c0979faadf0
steffkelsey <thecodeviking@gmail.com>
2026-04-20 08:28:23 -0400

    Fixes typer options

--- python/src/pymusicutils/pymsctl.py
+++ python/src/pymusicutils/pymsctl.py
@@ -3,21 +3,11 @@ from typing_extensions import Annotated
 
 
 def pymsctl(
-    name: Annotated[str, typer.Argument(help="The (last, if --title is given) name of the person to greet")] = "",
-    title: Annotated[str, typer.Option(help="The preferred title of the person to greet")] = "",
-    doctor: Annotated[bool, typer.Option(help="Whether the person is a doctor (MD or PhD)")] = False,
-    count: Annotated[int, typer.Option(help="Number of times to greet the person")] = 1
+    input_file: Annotated[str, typer.Option(help="The json report with albums to search for")],
+    output_dir: Annotated[str, typer.Option(help="The directory to size downloaded msuic files")],
+    dry_run: Annotated[bool, typer.Option(help="Dry-run to print json results to stdout")] = False
 ):
-    greeting = "Greetings, "
-    if doctor and not title:
-        title = "Dr."
-    if not name:
-        if title:
-            name = title.lower().rstrip(".")
-        else:
-            name = "friend"
-    if title:
-        greeting += f"{title} "
-    greeting += f"{name}!"
-    for i in range(0, count):
-        print(greeting)
+    result = f"input-file: {input_file}\n"
+    result += f"output-dir: {output_dir}\n"
+    result += f"is-dry-run: {dry_run}\n"
+    print(result)
